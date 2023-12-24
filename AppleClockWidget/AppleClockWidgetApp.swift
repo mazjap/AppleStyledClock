52cd9adc30de3ac67458d0cc8fb4da039803dacc
Jordan Christensen <jordan.c4922@gmail.com>
2023-12-23 17:26:29 -0700

    Initial Commit

---
+++ AppleClockWidget/AppleClockWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleClockWidgetApp.swift
+//  AppleClockWidget
+//
+//  Created by Jordan Christensen on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleClockWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
